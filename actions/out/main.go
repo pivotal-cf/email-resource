936dd70ae5cc9eac168e0bad84fcb9f4fbfde979
Gabriel Rosenhouse <grosenhouse@pivotal.io>
2015-08-04 14:14:34 -0700

    `out` reports subject and smtp_host
    [#100433254]
    
    Signed-off-by: Brian Cunnie <bcunnie@pivotal.io>

--- actions/out/main.go
+++ actions/out/main.go
@@ -3,16 +3,50 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"time"
 )
 
 func main() {
+
+	var indata struct {
+		Source struct {
+			SMTP struct {
+				Host string
+			}
+		}
+		Params struct {
+			Subject string
+		}
+	}
+
+	inbytes, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(inbytes, &indata)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "expected JSON input")
+		os.Exit(1)
+	}
+
+	type MetadataItem struct {
+		Name  string
+		Value string
+	}
 	var outdata struct {
 		Version struct {
 			Time time.Time
 		} `json:"version"`
+		Metadata []MetadataItem
 	}
 	outdata.Version.Time = time.Now().UTC()
+	outdata.Metadata = []MetadataItem{
+		{Name: "smtp_host", Value: indata.Source.SMTP.Host},
+		{Name: "subject", Value: indata.Params.Subject},
+	}
 	outbytes, err := json.Marshal(outdata)
 	if err != nil {
 		panic(err)
--- ci/out_test.go
+++ ci/out_test.go
@@ -9,12 +9,30 @@ import (
 )
 
 var _ = Describe("Out", func() {
+	var inputdata string
+	type MetadataItem struct {
+		Name  string
+		Value string
+	}
+
 	BeforeEach(func() {
 		Run("go", "build", "-o", "../bin/out", "../actions/out")
+		inputdata = `
+{
+  "source": {
+    "smtp": {
+			"host": "smtp.sendgrid.net"
+		}
+  },
+  "params": {
+		"subject": "/some/path/to/subject/line"
+  }
+}
+		`
 	})
 
-	It("should print the current time as a version and exit 0", func() {
-		output, err := RunWithStdinAllowError("some input data", "../bin/out", "some", "arguments")
+	It("should report the current time as a version and exit 0", func() {
+		output, err := RunWithStdinAllowError("{}", "../bin/out", "some", "arguments")
 		Expect(err).NotTo(HaveOccurred())
 
 		var outdata struct {
@@ -29,4 +47,25 @@ var _ = Describe("Out", func() {
 		Expect(json.Unmarshal([]byte(output), &untyped)).To(Succeed())
 		Expect(untyped).To(HaveKey("version"))
 	})
+
+	It("should report all the expected metadata fields", func() {
+		output, err := RunWithStdinAllowError(inputdata, "../bin/out")
+		Expect(err).NotTo(HaveOccurred())
+
+		var outdata struct {
+			Metadata []MetadataItem
+		}
+		Expect(json.Unmarshal([]byte(output), &outdata)).To(Succeed())
+		Expect(outdata.Metadata).To(ContainElement(Equal(MetadataItem{Name: "smtp_host", Value: "smtp.sendgrid.net"})))
+		Expect(outdata.Metadata).To(ContainElement(Equal(MetadataItem{Name: "subject", Value: "/some/path/to/subject/line"})))
+	})
+
+	Context("When the STDIN is not valid JSON", func() {
+		It("should print an error and exit 1", func() {
+			output, err := RunWithStdinAllowError("not JSON", "../bin/out", "some", "arguments")
+			Expect(err).To(HaveOccurred())
+			Expect(output).To(Equal("expected JSON input"))
+		})
+	})
+
 })
