89146bc8bc15c6894ea72ee382bf0fe559089df7
Matthew Boedicker <mboedicker@pivotal.io>
2016-02-10 17:03:46 -0800

    Add output to demo-prep-sha-email task
    [#113502075] Make sure all concourse jobs use task outputs
    
    Signed-off-by: Anoop Gopalakrishnan <agopalakrishnan@pivotal.io>

--- ci/demo-prep-sha-email.sh
+++ ci/demo-prep-sha-email.sh
@@ -2,5 +2,8 @@
 
 SHA=$(shasum $file_to_hash | cut -d ' ' -f1)
 
+output_body_file=email-out/$output_body_file
+output_subject_file=email-out/$output_subject_file
+
 echo -e "Email resource demo on $(date)" > $output_subject_file
 echo -e "SHA1 Hash of \"$file_to_hash\" is $SHA\n\nCheers!\n" > $output_body_file
