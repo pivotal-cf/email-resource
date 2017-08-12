880960615b00d79d5245bb219c00a179d1249b73
Caleb Washburn <cwashburn@pivotal.io>
2017-08-11 22:40:22 -0700

    move to glide instead of godeps

--- ci/tasks/runTests.sh
+++ ci/tasks/runTests.sh
@@ -4,10 +4,9 @@ export GOPATH=$PWD/go
 export PATH=$GOPATH/bin:$PATH
 
 go get github.com/Masterminds/glide
-go get golang.org/x/tools/cmd/cover
-go get github.com/onsi/ginkgo/ginkgo
 WORKING_DIR=$GOPATH/src/github.com/pivotal-cf/email-resource
 mkdir -p ${WORKING_DIR}
 cp -R source/* ${WORKING_DIR}/.
 cd ${WORKING_DIR}
+glide install
 go test $(glide nv) -v
