b83fdf4d8c967bce40e77019a19a0c451a59e3c6
Caleb Washburn <cwashburn@pivotal.io>
2018-11-06 11:27:21 -0700

    refactored to break out to helper functions

--- out/out.go
+++ out/out.go
@@ -21,14 +21,6 @@ import (
 func Execute(sourceRoot, version string, input []byte) (string, error) {
 
 	logger := log.New(os.Stderr, "", log.LstdFlags)
-	var buildTokens = map[string]string{
-		"${BUILD_ID}":            os.Getenv("BUILD_ID"),
-		"${BUILD_NAME}":          os.Getenv("BUILD_NAME"),
-		"${BUILD_JOB_NAME}":      os.Getenv("BUILD_JOB_NAME"),
-		"${BUILD_PIPELINE_NAME}": os.Getenv("BUILD_PIPELINE_NAME"),
-		"${ATC_EXTERNAL_URL}":    os.Getenv("ATC_EXTERNAL_URL"),
-		"${BUILD_TEAM_NAME}":     os.Getenv("BUILD_TEAM_NAME"),
-	}
 
 	if sourceRoot == "" {
 		return "", errors.New("expected path to build sources as first argument")
@@ -41,107 +33,59 @@ func Execute(sourceRoot, version string, input []byte) (string, error) {
 		return "", err
 	}
 
-	if indata.Source.SMTP.Host == "" {
-		return "", errors.New(`missing required field "source.smtp.host"`)
+	err = validateConfiguration(indata)
+	if err != nil {
+		return "", err
 	}
 
-	if indata.Source.SMTP.Port == "" {
-		return "", errors.New(`missing required field "source.smtp.port"`)
-	}
+	source := indata.Source
+	params := indata.Params
+	smtpConfig := source.SMTP
 
-	if indata.Source.From == "" {
-		return "", errors.New(`missing required field "source.from"`)
-	}
-
-	if len(indata.Source.To) == 0 && len(indata.Params.To) == 0 {
-		return "", errors.New(`missing required field "source.to" or "params.to". Must specify at least one`)
-	}
-
-	if indata.Params.Subject == "" && indata.Params.SubjectText == "" {
-		return "", errors.New(`missing required field "params.subject" or "params.subject_text". Must specify at least one`)
-	}
-
-	if indata.Source.SMTP.Anonymous == false {
-		if indata.Source.SMTP.Username == "" {
-			return "", errors.New(`missing required field "source.smtp.username" if anonymous specify anonymous: true`)
-		}
-
-		if indata.Source.SMTP.Password == "" {
-			return "", errors.New(`missing required field "source.smtp.password" if anonymous specify anonymous: true`)
-		}
-	}
-
-	replaceTokens := func(sourceString string) string {
-		for k, v := range buildTokens {
-			sourceString = strings.Replace(sourceString, k, v, -1)
-		}
-		return sourceString
-	}
-
-	readSource := func(sourcePath string) (string, error) {
-		if !filepath.IsAbs(sourcePath) {
-			sourcePath = filepath.Join(sourceRoot, sourcePath)
-		}
-		var bytes []byte
-		bytes, err = ioutil.ReadFile(sourcePath)
-		return replaceTokens(string(bytes)), err
-	}
-
-	fromTextOrFile := func(text string, filePath string) (string, error) {
-		if text != "" {
-			return replaceTokens(text), nil
-
-		}
-		if filePath != "" {
-			return readSource(filePath)
-		}
-		return "", nil
-	}
-
-	subject, err := fromTextOrFile(indata.Params.SubjectText, indata.Params.Subject)
+	subject, err := fromTextOrFile(sourceRoot, params.SubjectText, params.Subject)
 	if err != nil {
 		return "", err
 	}
 	subject = strings.Trim(subject, "\n")
 
 	var headers string
-	if indata.Params.Headers != "" {
-		headers, err = readSource(indata.Params.Headers)
+	if params.Headers != "" {
+		headers, err = readSource(sourceRoot, params.Headers)
 		if err != nil {
 			return "", errors.Wrap(err, "unable to read source file for headers")
 		}
 		headers = strings.Trim(headers, "\n")
 	}
 
-	body, err := fromTextOrFile(indata.Params.BodyText, indata.Params.Body)
+	body, err := fromTextOrFile(sourceRoot, params.BodyText, params.Body)
 	if err != nil {
 		return "", err
 	}
 
-	if indata.Params.To != "" {
+	if params.To != "" {
 		var toList string
-		toList, err = readSource(indata.Params.To)
+		toList, err = readSource(sourceRoot, params.To)
 		if err != nil {
 			return "", err
 		}
 		if len(toList) > 0 {
 			toListArray := strings.Split(toList, ",")
 			for _, toAddress := range toListArray {
-				indata.Source.To = append(indata.Source.To, strings.TrimSpace(toAddress))
+				source.To = append(source.To, strings.TrimSpace(toAddress))
 			}
 		}
 	}
 
-	if indata.Params.Bcc != "" {
+	if params.Bcc != "" {
 		var bccList string
-		bccList, err = readSource(indata.Params.Bcc)
+		bccList, err = readSource(sourceRoot, params.Bcc)
 		if err != nil {
 			return "", err
 		}
 		if len(bccList) > 0 {
 			bccListArray := strings.Split(bccList, ",")
 			for _, bccAddress := range bccListArray {
-				indata.Source.Bcc = append(indata.Source.Bcc, strings.TrimSpace(bccAddress))
+				source.Bcc = append(source.Bcc, strings.TrimSpace(bccAddress))
 			}
 		}
 	}
@@ -149,7 +93,7 @@ func Execute(sourceRoot, version string, input []byte) (string, error) {
 	var outdata Output
 	outdata.Version.Time = time.Now().UTC()
 	outdata.Metadata = []MetadataItem{
-		{Name: "smtp_host", Value: indata.Source.SMTP.Host},
+		{Name: "smtp_host", Value: smtpConfig.Host},
 		{Name: "subject", Value: subject},
 		{Name: "version", Value: version},
 	}
@@ -158,13 +102,13 @@ func Execute(sourceRoot, version string, input []byte) (string, error) {
 		return "", err
 	}
 
-	if indata.Params.SendEmptyBody == false && len(body) == 0 {
+	if params.SendEmptyBody == false && len(body) == 0 {
 		logger.Println("Message not sent because the message body is empty and send_empty_body parameter was set to false. Github readme: https://github.com/pivotal-cf/email-resource")
 		return string(outbytes), nil
 	}
 	var messageData []byte
-	messageData = append(messageData, []byte("To: "+strings.Join(indata.Source.To, ", ")+"\n")...)
-	messageData = append(messageData, []byte("From: "+indata.Source.From+"\n")...)
+	messageData = append(messageData, []byte("To: "+strings.Join(source.To, ", ")+"\n")...)
+	messageData = append(messageData, []byte("From: "+source.From+"\n")...)
 	if headers != "" {
 		messageData = append(messageData, []byte(headers+"\n")...)
 	}
@@ -175,7 +119,7 @@ func Execute(sourceRoot, version string, input []byte) (string, error) {
 
 	var c *smtp.Client
 	var wc io.WriteCloser
-	c, err = smtp.Dial(fmt.Sprintf("%s:%s", indata.Source.SMTP.Host, indata.Source.SMTP.Port))
+	c, err = smtp.Dial(fmt.Sprintf("%s:%s", smtpConfig.Host, smtpConfig.Port))
 	if err != nil {
 		return "", err
 	}
@@ -183,55 +127,33 @@ func Execute(sourceRoot, version string, input []byte) (string, error) {
 
 	hostOrigin := "localhost"
 
-	if indata.Source.SMTP.HostOrigin != "" {
-		hostOrigin = indata.Source.SMTP.HostOrigin
+	if smtpConfig.HostOrigin != "" {
+		hostOrigin = smtpConfig.HostOrigin
 	}
 	if err = c.Hello(hostOrigin); err != nil {
 		return "", errors.Wrap(err, fmt.Sprintf("unable to connect with hello with host name %s, try setting property host_origin", hostOrigin))
 	}
 	if ok, _ := c.Extension("STARTTLS"); ok {
-		config := tlsConfig(indata)
+		config := tlsConfig(smtpConfig)
 
 		if err = c.StartTLS(config); err != nil {
 			return "", errors.Wrap(err, "unable to start TLS")
 		}
 	}
 
-	if indata.Source.SMTP.LoginAuth {
-
-		auth := LoginAuth(indata.Source.SMTP.Username, indata.Source.SMTP.Password)
-
-		if auth != nil {
-			if ok, _ := c.Extension("AUTH"); ok {
-				if err = c.Auth(auth); err != nil {
-					return "", errors.Wrap(err, "unable to auth using type Login Auth")
-				}
-			}
-		}
-	} else if !indata.Source.SMTP.Anonymous && !indata.Source.SMTP.LoginAuth {
-		auth := smtp.PlainAuth(
-			"",
-			indata.Source.SMTP.Username,
-			indata.Source.SMTP.Password,
-			indata.Source.SMTP.Host,
-		)
-		if auth != nil {
-			if ok, _ := c.Extension("AUTH"); ok {
-				if err = c.Auth(auth); err != nil {
-					return "", errors.Wrap(err, "unable to auth using type Plain Auth")
-				}
-			}
-		}
-	}
-	if err = c.Mail(indata.Source.From); err != nil {
+	err = doAuth(smtpConfig, c)
+	if err != nil {
 		return "", err
 	}
-	for _, addr := range indata.Source.To {
+	if err = c.Mail(source.From); err != nil {
+		return "", err
+	}
+	for _, addr := range source.To {
 		if err = c.Rcpt(addr); err != nil {
 			return "", err
 		}
 	}
-	for _, addr := range indata.Source.Bcc {
+	for _, addr := range source.Bcc {
 		if err = c.Rcpt(addr); err != nil {
 			return "", err
 		}
@@ -256,19 +178,84 @@ func Execute(sourceRoot, version string, input []byte) (string, error) {
 	return string(outbytes), err
 }
 
-func tlsConfig(indata Input) *tls.Config {
-	config := &tls.Config{
-		ServerName: indata.Source.SMTP.Host,
+func validateConfiguration(indata Input) error {
+	if indata.Source.SMTP.Host == "" {
+		return errors.New(`missing required field "source.smtp.host"`)
 	}
 
-	if indata.Source.SMTP.SkipSSLValidation {
-		config.InsecureSkipVerify = indata.Source.SMTP.SkipSSLValidation
+	if indata.Source.SMTP.Port == "" {
+		return errors.New(`missing required field "source.smtp.port"`)
+	}
+
+	if indata.Source.From == "" {
+		return errors.New(`missing required field "source.from"`)
+	}
+
+	if len(indata.Source.To) == 0 && len(indata.Params.To) == 0 {
+		return errors.New(`missing required field "source.to" or "params.to". Must specify at least one`)
+	}
+
+	if indata.Params.Subject == "" && indata.Params.SubjectText == "" {
+		return errors.New(`missing required field "params.subject" or "params.subject_text". Must specify at least one`)
+	}
+
+	if indata.Source.SMTP.Anonymous == false {
+		if indata.Source.SMTP.Username == "" {
+			return errors.New(`missing required field "source.smtp.username" if anonymous specify anonymous: true`)
+		}
+
+		if indata.Source.SMTP.Password == "" {
+			return errors.New(`missing required field "source.smtp.password" if anonymous specify anonymous: true`)
+		}
+	}
+	return nil
+}
+
+func doAuth(smtpConfig SMTP, c *smtp.Client) error {
+	if smtpConfig.Anonymous {
+		return nil
+	}
+	if smtpConfig.LoginAuth {
+		auth := LoginAuth(smtpConfig.Username, smtpConfig.Password)
+
+		if auth != nil {
+			if ok, _ := c.Extension("AUTH"); ok {
+				if err := c.Auth(auth); err != nil {
+					return errors.Wrap(err, "unable to auth using type Login Auth")
+				}
+			}
+		}
+	} else {
+		auth := smtp.PlainAuth(
+			"",
+			smtpConfig.Username,
+			smtpConfig.Password,
+			smtpConfig.Host,
+		)
+		if auth != nil {
+			if ok, _ := c.Extension("AUTH"); ok {
+				if err := c.Auth(auth); err != nil {
+					return errors.Wrap(err, "unable to auth using type Plain Auth")
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func tlsConfig(smtpConfig SMTP) *tls.Config {
+	config := &tls.Config{
+		ServerName: smtpConfig.Host,
+	}
+
+	if smtpConfig.SkipSSLValidation {
+		config.InsecureSkipVerify = smtpConfig.SkipSSLValidation
 		return config
 	}
 
-	if indata.Source.SMTP.CaCert != "" {
+	if smtpConfig.CaCert != "" {
 		caPool := x509.NewCertPool()
-		caPool.AppendCertsFromPEM([]byte(indata.Source.SMTP.CaCert))
+		caPool.AppendCertsFromPEM([]byte(smtpConfig.CaCert))
 
 		config.RootCAs = caPool
 
@@ -276,4 +263,38 @@ func tlsConfig(indata Input) *tls.Config {
 	}
 
 	return config
+}
+
+func replaceTokens(sourceString string) string {
+	var buildTokens = map[string]string{
+		"${BUILD_ID}":            os.Getenv("BUILD_ID"),
+		"${BUILD_NAME}":          os.Getenv("BUILD_NAME"),
+		"${BUILD_JOB_NAME}":      os.Getenv("BUILD_JOB_NAME"),
+		"${BUILD_PIPELINE_NAME}": os.Getenv("BUILD_PIPELINE_NAME"),
+		"${ATC_EXTERNAL_URL}":    os.Getenv("ATC_EXTERNAL_URL"),
+		"${BUILD_TEAM_NAME}":     os.Getenv("BUILD_TEAM_NAME"),
+	}
+	for k, v := range buildTokens {
+		sourceString = strings.Replace(sourceString, k, v, -1)
+	}
+	return sourceString
+}
+
+func readSource(sourceRoot, sourcePath string) (string, error) {
+	if !filepath.IsAbs(sourcePath) {
+		sourcePath = filepath.Join(sourceRoot, sourcePath)
+	}
+	bytes, err := ioutil.ReadFile(sourcePath)
+	return replaceTokens(string(bytes)), err
+}
+
+func fromTextOrFile(sourceRoot, text, filePath string) (string, error) {
+	if text != "" {
+		return replaceTokens(text), nil
+
+	}
+	if filePath != "" {
+		return readSource(sourceRoot, filePath)
+	}
+	return "", nil
 }
--- out/types.go
+++ out/types.go
@@ -4,33 +4,39 @@ import "time"
 
 //Input - Struct that represents the input to out
 type Input struct {
-	Source struct {
-		SMTP struct {
-			Host              string
-			Port              string
-			Username          string
-			Password          string
-			Anonymous         bool   `json:"anonymous"`
-			SkipSSLValidation bool   `json:"skip_ssl_validation"`
-			CaCert            string `json:"ca_cert"`
-			HostOrigin        string `json:"host_origin"`
-			LoginAuth         bool   `json:"login_auth"`
-		}
-		From string
-		To   []string
-		Bcc  []string
-	}
-	Params struct {
-		Subject       string
-		SubjectText   string `json:"subject_text"`
-		Body          string
-		BodyText      string `json:"body_text"`
-		SendEmptyBody bool   `json:"send_empty_body"`
-		Headers       string
-		HeadersText   string `json:"headers_text"`
-		To            string `json:"to"`
-		Bcc           string `json:"bcc"`
-	}
+	Source Source `json:"source"`
+	Params Params `json:"params"`
+}
+
+type Source struct {
+	SMTP SMTP `json:"smtp"`
+	From string
+	To   []string
+	Bcc  []string
+}
+
+type Params struct {
+	Subject       string
+	SubjectText   string `json:"subject_text"`
+	Body          string
+	BodyText      string `json:"body_text"`
+	SendEmptyBody bool   `json:"send_empty_body"`
+	Headers       string
+	HeadersText   string `json:"headers_text"`
+	To            string `json:"to"`
+	Bcc           string `json:"bcc"`
+}
+
+type SMTP struct {
+	Host              string
+	Port              string
+	Username          string
+	Password          string
+	Anonymous         bool   `json:"anonymous"`
+	SkipSSLValidation bool   `json:"skip_ssl_validation"`
+	CaCert            string `json:"ca_cert"`
+	HostOrigin        string `json:"host_origin"`
+	LoginAuth         bool   `json:"login_auth"`
 }
 
 //MetadataItem - metadata within output
